39be1aa4ab8a94702948df277085253327da47e3
Samuele Orfei <fufoed89@gmail.com>
2023-10-20 19:36:09 +0200

    ADD: Axiom generation (Algorithm first step)

--- src/Algorithm/Algorithm.fs
+++ src/Algorithm/Algorithm.fs
@@ -4,11 +4,22 @@ open Models
 open Types
 
 module Calculus =
-    let generateAxioms (f: Formula) : Formula list =
-        let (sl, sr) = Expression.subFormulas f
 
-        let atomsSL = Expression.atoms sl
-        let atomsSR = Expression.atoms sr
+
+    let generateAxioms (f: Formula) : (Formula list * Formula list) list =
+        let extraction (set: Formula list) : Set<Formula> =
+            let rec filter (acc: Set<Formula>, fl: Formula list) =
+                match fl with
+                | [] -> acc
+                | x :: xs ->
+                    match x with
+                    | True
+                    | False
+                    | Atom(_)
+                    | Imp(_, _) -> filter (Set.add x acc, xs)
+                    | _ -> filter (acc, xs)
+
+            filter (Set.empty, set)
 
         let isValid (l: Set<Formula>, r: Set<Formula>, total: Set<Formula>) =
             let intersectionRule = (Set.intersect l r = Set.empty)
@@ -16,10 +27,21 @@ module Calculus =
 
             intersectionRule && unionRule
 
-        // Generare gli assiomi (Funzione incompleta, solo preparata)
+        let (sl, sr) = Expression.subFormulas f
 
-        match isValid (atomsSL, atomsSR, Set.union atomsSL atomsSR) with
-        | true -> (Set.union atomsSL atomsSR) |> Set.toList
+        let atomsSL = extraction sl
+        let atomsSR = extraction sr
+
+        let union = Set.union atomsSL atomsSR
+
+        match isValid (atomsSL, atomsSR, union) with
+        | true ->
+            let rec combinations (acc: (Formula list * Formula list) list, current: Formula list, set: Formula list) =
+                match set with
+                | [] -> acc @ [ (current, set) ]
+                | x :: xs -> combinations (acc @ [ (current, set) ], current @ [ x ], xs)
+
+            combinations ([], [], (Set.toList union))
         | false -> []
 
     let execute (goal: Formula, premises: Formula list) : Formula list = []
--- src/Algorithm/Algorithm.fsi
+++ src/Algorithm/Algorithm.fsi
@@ -6,6 +6,23 @@ open Models.Types
 /// refutazione nella logica di Godel-Dummet.
 module Calculus =
     ///
+    /// La funzione <code>generateAxioms</code> riceve in input una formula che rappresenta l'obiettivo del processo dimostrativo,
+    /// e restituisce un insieme di assiomi che rappresentano tutte le combinazioni di inferenza
+    ///
+    /// ---
+    ///
+    /// **Parameters**
+    ///
+    /// - <code>Formula</code> goal
+    /// ---
+    ///
+    /// **Return**
+    ///
+    /// - <code>(Formula list * Formula list) list</code>
+    ///
+    val generateAxioms: Formula -> (Formula list * Formula list) list
+
+    ///
     /// La funzione <code>calculate</code> riceve in input una formula che rappresenta l'obiettivo del processo dimostrativo,
     /// una lista di formule che constituiscono le premesse da cui partire e restituisce una lista di formule che rappresentano
     /// gli assiomi validi
@@ -23,5 +40,3 @@ module Calculus =
     /// - <code>Formula list</code>
     ///
     val execute: Formula * Formula list -> Formula list
-
-    val generateAxioms: Formula -> Formula list
